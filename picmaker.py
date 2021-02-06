836ec69e7d6921e12dd89109fe477af01efe6cd7
Anna Zhang <anna@MacBook-Pro-10.fios-router.home>
2021-02-05 21:45:08 -0500

    mod 256

--- picmaker.py
+++ picmaker.py
@@ -6,6 +6,7 @@ def makepic():
         for j in range(500):
             x = i if i % 2 == 0 else i + 1
             l = [x ** 2 + j ** 2 + x * j, x ** 2 + j ** 2 + j, x ** 2 + j ** 2 + x]
+            l = [a % 256 for a in l]
             pic.write(rgb.format(a = l[0], b = l[1], c = l[2]))
         pic.write('\n')
     pic.close()
