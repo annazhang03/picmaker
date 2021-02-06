df05f96426a0860ec1dea085f6435fedda2aa219
Anna Zhang <anna@MacBook-Pro-10.fios-router.home>
2021-02-05 21:22:16 -0500

    modified picmaker.py

--- picmaker.py
+++ picmaker.py
@@ -1,11 +1,12 @@
-# create 500x500 image
 def makepic():
     rgb = "{a} {b} {c} "
     pic = open('image1.ppm', 'w')
     pic.write('P3 500 500 255\n')
     for i in range(500):
         for j in range(500):
-            l = [i ** 2 + j ** 2 + i * j, i ** 2 + j ** 2 + j + 200, i ** 2 + j ** 2 + i + 200]
+            x = i if i % 2 == 0 else i + 1
+            y = j if j % 2 == 0 else j + 1
+            l = [x ** 2 + y ** 2 + x * y, x ** 2 + y ** 2 + y, x ** 2 + y ** 2 + x]
             pic.write(rgb.format(a = l[0], b = l[1], c = l[2]))
         pic.write('\n')
     pic.close()
